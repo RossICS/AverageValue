62a2571097a8f7d5c2df09df1119ecd178ffb2ba
rkaune <rkaune@ugcloud.ca>
2018-03-27 13:09:48 -0400

    Average value

--- src/averagevalue/AverageValue.java
+++ src/averagevalue/AverageValue.java
@@ -23,21 +23,21 @@ import java.util.Scanner;
 		
 		/* Get a set of numbers from user */
 		System.out.println("Calculate Average Program");
-//		System.out.print("Enter a value (" + SENTINEL + " to quit): ");
-//		newValue = input.nextInt();
-//		//int num = -1;
-//		while (newValue != SENTINEL) {
-//			numValues += 1;
-//			sumOfValues += newValue;
-//			System.out.print("Enter a value (" + SENTINEL + " to quit): ");
-//			newValue = input.nextInt();
-//		}
-                for (int i=1;i<=4;i++){
-                    System.out.print("Enter a value:  ");
-                    newValue = input.nextInt();
-                    numValues += 1;
-                    sumOfValues += newValue;
-                }
+		System.out.print("Enter a value (" + SENTINEL + " to quit): ");
+		newValue = input.nextInt();
+		//int num = -1;
+		while (newValue != SENTINEL) {
+			numValues += 1;
+			sumOfValues += newValue;
+			System.out.print("Enter a value (" + SENTINEL + " to quit): ");
+			newValue = input.nextInt();
+		}
+//                for (int i=1;i<=4;i++){
+//                    System.out.print("Enter a value:  ");
+//                    newValue = input.nextInt();
+//                    numValues += 1;
+//                    sumOfValues += newValue;
+//                }
 		input.close();
 		
 		/*Calculate average of numbers entered by user */
